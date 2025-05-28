1afd13bb00ef7db3ef5f26fa22e5bc5607fd9d23
mvanpractice <michaelmanggay@gmail.com>
2025-05-28 14:46:15 +0800

    Answers for first TOP JS exercises til 06

--- 03_numberChecker/numberChecker.js
+++ 03_numberChecker/numberChecker.js
@@ -1,10 +1,18 @@
 function numberChecker(number) {
-  if (number === 6) {
+  if (number >= 10) {
     return true;
   } else {
     return false;
   }
 }
+
+// function numberChecker(number) {
+//   if (number === 1000 || number === 10) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// } - This is my initial dumb solution
 
 // Do not edit below this line
 module.exports = numberChecker;
--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(theString = '', theNumber) {
 
+    let res = '';
+
+    if (theNumber > 0) {
+        for (let i = 0; i < theNumber; i++) {
+            res = res + theString;
+        }
+    } else if (theNumber === 0) {
+        res = '';
+    } else {
+        res = 'ERROR';
+    }
+
+    return res;
+    
 };
 
 // Do not edit below this line
